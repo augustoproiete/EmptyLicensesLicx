250599d261b984a97bd2182b8407edf39ef01a7a
Caio Proiete <caio.proiete@gmail.com>
2018-02-13 13:57:31 -0400

    Add `CSharp` prefix to project names

--- tests/CSharpConsoleAppTestHarness/Properties/AssemblyInfo.cs
+++ tests/CSharpConsoleAppTestHarness/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleAppTestHarness")]
+[assembly: AssemblyTitle("CSharpConsoleAppTestHarness")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleAppTestHarness")]
+[assembly: AssemblyProduct("CSharpConsoleAppTestHarness")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- tests/CSharpWinFormsAppTestHarness/Properties/AssemblyInfo.cs
+++ tests/CSharpWinFormsAppTestHarness/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WinFormsAppTestHarness")]
+[assembly: AssemblyTitle("CSharpWinFormsAppTestHarness")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WinFormsAppTestHarness")]
+[assembly: AssemblyProduct("CSharpWinFormsAppTestHarness")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

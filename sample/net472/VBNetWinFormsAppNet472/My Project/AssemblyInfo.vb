708234b4141a58d5d612b3f2cee58c2736c21583
Caio Proiete <caio.proiete@gmail.com>
2019-11-16 17:47:33 -0400

    Reorganize projects and prep for .NET Core 3.0 samples

---
+++ sample/net472/VBNetWinFormsAppNet472/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("VBNetWinFormsAppNet472")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("VBNetWinFormsAppNet472")>
+<Assembly: AssemblyCopyright("Copyright © Caio Proiete & Contributors 2016-2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2e96959d-74a2-42da-94b0-b1eb506c9704")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyInformationalVersion("1.0.0.0")>
